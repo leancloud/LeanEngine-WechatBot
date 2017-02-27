06caff208bb30f1d73c7354d163506ac8ea1330c
Da Li <myleslee@gmail.com>
2017-02-27 10:29:45 +0800

    feat: 升级至 LeanEngine 1.x
    使用新的 node-js-getting-started 应用模板
    
    Closes #9

--- app.js
+++ app.js
@@ -1,49 +1,37 @@
 'use strict';
-var domain = require('domain');
 var express = require('express');
+var timeout = require('connect-timeout');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var wechat = require('./routes/wechatBot');
-var cloud = require('./cloud');
+var AV = require('leanengine');
+
+// 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
+require('./cloud');
 
 var app = express();
 
-// 设置 view 引擎
+// 设置模板引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
-// 加载云代码方法
-app.use(cloud);
+// 设置默认超时时间
+app.use(timeout('15s'));
+
+// 加载云引擎中间件
+app.use(AV.express());
+
+app.enable('trust proxy');
+// 需要重定向到 HTTPS 可去除下一行的注释。
+//app.use(AV.Cloud.HttpsRedirect());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-
-// 未处理异常捕获 middleware
-app.use(function(req, res, next) {
-  var d = null;
-  if (process.domain) {
-    d = process.domain;
-  } else {
-    d = domain.create();
-  }
-  d.add(req);
-  d.add(res);
-  d.on('error', function(err) {
-    console.error('uncaughtException url=%s, msg=%s', req.url, err.stack ||
-      err.message || err);
-    if (!res.finished) {
-      res.statusCode = 500;
-      res.setHeader('content-type', 'application/json; charset=UTF-8');
-      res.end('uncaughtException');
-    }
-  });
-  d.run(next);
-});
 
 app.get('/', function(req, res) {
   res.render('index', {
@@ -52,39 +40,41 @@ app.get('/', function(req, res) {
 });
 
 // 可以将一类的路由单独保存在一个文件中
-app.use('/wechat', wechat);
+app.use('/wechat', require('./routes/wechatBot'));
 
-// 如果任何路由都没匹配到，则认为 404
-// 生成一个异常让后面的 err handler 捕获
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
+  if (!res.headersSent) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  }
 });
 
 // error handlers
+app.use(function(err, req, res, next) {
+  if (req.timedout && req.headers.upgrade === 'websocket') {
+    // 忽略 websocket 的超时
+    return;
+  }
 
-// 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) { // jshint ignore:line
-    var statusCode = err.status || 500;
-    if (statusCode === 500) {
-      console.error(err.stack || err);
-    }
-    res.status(statusCode);
-    res.render('error', {
-      message: err.message || err,
-      error: err
-    });
-  });
-}
-
-// 如果是非开发环境，则页面只输出简单的错误信息
-app.use(function(err, req, res, next) { // jshint ignore:line
-  res.status(err.status || 500);
+  var statusCode = err.status || 500;
+  if (statusCode === 500) {
+    console.error(err.stack || err);
+  }
+  if (req.timedout) {
+    console.error('请求超时: url=%s, timeout=%d, 请确认方法执行耗时很长，或没有正确的 response 回调。', req.originalUrl, err.timeout);
+  }
+  res.status(statusCode);
+  // 默认不输出异常详情
+  var error = {}
+  if (app.get('env') === 'development') {
+    // 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
+    error = err;
+  }
   res.render('error', {
-    message: err.message || err,
-    error: {}
+    message: err.message,
+    error: error
   });
 });
 
